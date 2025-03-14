381e07db5283e4af1405b87cf54f2227f8fa6db2
Gavin Hughes <49838674+RedstoneEngine@users.noreply.github.com>
2025-03-14 01:41:08 -0500

    Formatting Changes
    Need to still update dropdown code and clean up show page code

---
+++ assets/scripts/hughesWebsite.js
@@ -0,0 +1,8 @@
+
+// Change icon and title
+document.title = "Gavin Hughes"
+
+var icons = document.getElementsByClassName('icon');
+for (var i = 0; i < icons.length; i++) {
+    icons[i].href = "./assets/sprites/WebsiteIcon_Hughes.ico";
+}
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,20 +1,64 @@
 
+// Main JS Functions
+
 // Assign Element Variables
 document.addEventListener("DOMContentLoaded", () => {
     banner = document.getElementById("banner");
     navBar = document.getElementById("navBar");
     shortNavBar = document.getElementById("shortNavBar");
     navBarSpacer = document.getElementById("navBarSpacer");
+
+    // Load current Page
+    var hashLocation = document.location.hash;
+    if (hashLocation != null) {
+        // Portfolio Page Load
+        var isPorfolio = false;
+        if (hashLocation.endsWith("Portfolio")) {
+            isPorfolio = true;
+            hashLocation = hashLocation.substring(0, hashLocation.length - 9);
+            document.getElementById("navBar").classList.add("hide");
+            document.getElementById("shortNavBar").classList.add("hide");
+            document.getElementById("navBarSpacer").classList.add("hide");
+
+            var pages = document.getElementsByClassName('page');
+            for (var i = 0; i < pages.length; i++) {
+                pages[i].classList.add("pageFullHeight");
+            }
+        }
+        // Standard Page Load
+        if (document.getElementById(hashLocation.substring(1) + "_") != null) {
+            if (!isPorfolio)
+                document.getElementById("WhoAmI?_").classList.remove("showPage")
+            document.getElementById(hashLocation.substring(1) + "_").classList.add("showPage");
+            document.getElementById("navBarCurrent").firstChild.textContent = hashLocation.substring(1);
+            for (var i = 0; i < document.getElementById("navBar").children.length; i++) {
+                var child = document.getElementById("navBar").children[i];
+                if (i == 0)
+                    child.classList.remove("selected");
+                if (child.children[0].innerHTML.startsWith(hashLocation.substring(1, 4))) {
+                    child.classList.add("selected");
+                }
+            }
+        }
+        else {
+            document.location.hash = '';
+        }
+    }
 });
 
 // Overlay functions
-function showOverlay(show, overlayId, event) {
-    if (show) {
-        document.getElementById(overlayId).classList.add("showOverlay");
-        document.getElementById(overlayId).onclick = function(e) {showOverlay(false, overlayId, e)};
+function showOverlay(showId) {
+    if (!document.getElementById("overlay").classList.contains("showOverlay")) {
+        document.getElementById("overlay").classList.add("showOverlay");
+        document.getElementById("overlay").onclick = function(e) {showOverlay(showId)};
+        document.getElementById(showId).classList.add("show");
+        document.getElementById(showId).parentElement.style.zIndex = 11;
     }
-    else if (!event || event.target.classList.contains("overlay"))
-        document.getElementById(overlayId).classList.remove("showOverlay");
+    else {
+        document.getElementById("overlay").classList.remove("showOverlay");
+        document.getElementById(showId).classList.remove("show");
+        document.getElementById(showId).parentElement.style.zIndex = null;
+    }
 }
 
 // Nav Button Sticky Header
@@ -38,6 +82,9 @@ window.addEventListener("scroll", () => {
 // Nav Button Selection
 function showPage(self) {
 
+    if (window.scrollY >= banner.clientHeight)
+        window.scrollTo(0, banner.clientHeight);
+
     var prevIndex = -1;
     var currIndex = -1;
     var prevPage = null;
@@ -46,36 +93,49 @@ function showPage(self) {
     // Find Last Selection
     for (var i = 0; i < document.getElementById("navBar").children.length; i++) {
         var child = document.getElementById("navBar").children[i];
-        if (child.classList.contains("selected")) {
+        if (child.children[0] == self)
+            // Track Current index
+            currIndex = i;
+        else if (child.classList.contains("selected")) {
             // Remove Button Selection
             child.classList.remove("selected");
             // Hide Previous Page
-            prevPage = document.getElementById(child.children[0].innerHTML);
+            prevPage = document.getElementById(child.children[0].innerHTML.replaceAll(' ', '') + "_");
             setTimeout(() => {prevPage.classList.remove("showPage")}, 250);
             // Grab index
             prevIndex = i;
         }
-        if (child.children[0] == self)
-            // Track Current index
-            currIndex = i;
     }
 
     // Enable Button Selection
     self.parentNode.classList.add("selected");
     // Show Corresponding Page
-    currPage = document.getElementById(self.innerHTML);
+    var pageName = self.innerHTML.replaceAll(' ', '');
+    currPage = document.getElementById(pageName + "_");
+    document.location.hash = pageName;
     currPage.classList.add("showPage");
 
+    document.getElementById("navBarCurrent").firstChild.textContent = self.innerHTML;
+
     // Animations
-    if (currIndex > prevIndex) {
-        prevPage.style.animation = "slideInFromLeft .25s ease-in-out 0s 1 reverse forwards";
-        currPage.style.animation = "slideInFromRight .25s ease-in-out 0s 1 normal forwards";
+    if (prevIndex != -1) {
+        if (currIndex > prevIndex) {
+            prevPage.style.animation = "slideInFromLeft .25s ease-in-out 0s 1 reverse forwards";
+            currPage.style.animation = "slideInFromRight .25s ease-in-out 0s 1 normal forwards";
+        }
+        else {
+            prevPage.style.animation = "slideInFromRight .25s ease-in-out 0s 1 reverse forwards";
+            currPage.style.animation = "slideInFromLeft .25s ease-in-out 0s 1 normal forwards";
+        }
+        // Reset
+        setTimeout(() => {prevPage.style.animation = ""}, 250);
+        setTimeout(() => {currPage.style.animation = ""}, 250);
     }
-    else {
-        prevPage.style.animation = "slideInFromRight .25s ease-in-out 0s 1 reverse forwards";
-        currPage.style.animation = "slideInFromLeft .25s ease-in-out 0s 1 normal forwards";
-    }
-    // Reset
-    setTimeout(() => {prevPage.style.animation = ""}, 250);
-    setTimeout(() => {currPage.style.animation = ""}, 250);
+}
+
+// Dropdown Page Selection
+function dropdownShowPage(index) {
+    console.log(index);
+    document.getElementById("navBar").children[index].children[0].click();
+    showOverlay("navList");
 }
