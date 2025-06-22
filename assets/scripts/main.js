2827be214c9a426b7385aa69a0ad5a62c37bf9c6
Gavin Hughes <49838674+RedstoneEngine@users.noreply.github.com>
2025-06-22 01:16:08 -0500

    Big Cleanup Update
    Added What I'm Up To Video section
    Added Monty's Website section
    Update OtherProjects section
    Reloading on DemoProjects will save on hash
    Image size improvements
    Printing improvements
    Other small changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,6 +16,12 @@ document.addEventListener("DOMContentLoaded", () => {
     if (hashLocation != null) {
         var isPorfolio = false;
 
+        var subDomain = "";
+        if (hashLocation.split('_').length > 1) {
+            subDomain = hashLocation.split('_')[1];
+            hashLocation = hashLocation.split('_')[0];
+        }
+
         // Portfolio Page Load
         if (hashLocation.endsWith("Portfolio")) {
             isPorfolio = true;
@@ -43,7 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
                 document.getElementById(pageTitles[0].replaceAll(' ', '') + "_").classList.remove("showPage")
 
             // Show Current Page
-            document.getElementById(hashLocation + "_").classList.add("showPage");
+            var page = document.getElementById(hashLocation + "_");
+            page.classList.add("showPage");
             
             // Highlight Correct Button
             document.getElementById("navBar").children[0].classList.remove("selected");
@@ -54,13 +61,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
             if (hashLocation == "Don'tPress!")
                 randomPressed();
+            if (hashLocation == "WhoAmI?" || isPorfolio)
+                aboutMePressed();
+
+            // Loads subDomain
+            if (subDomain != "") {
+                page.children[2].children[0].classList.remove("selected");
+                page.children[2].children[1].classList.add("selected");
+                loadSubDomain(page, true);
+            }
         }
         // Incorrect Load
         else {
             document.location.hash = '';
+            aboutMePressed();
         }
     }
+    else
+        aboutMePressed();
+
+    window.addEventListener("scroll", () => scrollResponse());
+    scrollResponse();
+
+    setupFavoriteVideos();
 });
+
+function aboutMePressed() {
+    document.getElementById("aboutMeFrame").src = "https://www.youtube.com/embed/M6HiwVCEiQA";
+}
 
 // Overlay functions
 function showOverlay(showId, anchor) {
@@ -80,7 +108,8 @@ function showOverlay(showId, anchor) {
         document.getElementById(showId).classList.remove("show");
         document.body.style.overflowY = "auto";
         // Disable Video just in case
-        document.getElementById("galleryImage").children[1].src = null;
+        if (document.getElementById("galleryImage"))
+            document.getElementById("galleryImage").children[1].src = "";
     }
 }
 
@@ -89,7 +118,7 @@ var banner = null;
 var navBar = null;
 var shortNavBar = null;
 var navBarSpacer = null;
-window.addEventListener("scroll", () => {
+function scrollResponse() {
     if (window.scrollY >= banner.clientHeight + 20) {
         navBar.classList.add("navBarLock");
         shortNavBar.classList.add("navBarLock");
@@ -100,7 +129,7 @@ window.addEventListener("scroll", () => {
         shortNavBar.classList.remove("navBarLock");
         navBarSpacer.style.display = "none";
     }
-});
+}
 
 // Nav Button Selection
 var prevIndex = 0;
@@ -173,22 +202,30 @@ function switchPanel(self) {
 
 // Engineering Code
 function showGallery(self) {
-    // Show Image
-    if (!self.style.backgroundImage.includes("_Thnl")) {
-        document.getElementById("galleryImage").children[0].src = self.style.backgroundImage.substring(5, self.style.backgroundImage.length - 2);
-        document.getElementById("galleryImage").children[0].style.display = "inherit";
-        document.getElementById("galleryImage").children[1].style.display = "none";
-    }
-    else {
-        var src = self.style.backgroundImage.substring(5, self.style.backgroundImage.length - 11) + ".mp4";
+    // Show Video
+    if (self.style.backgroundImage.includes("_Thnl") || self.hasAttribute("youtube")) {
+        var src = self.style.backgroundImage.substring(5, self.style.backgroundImage.length - 12) + ".mp4";
         if (self.hasAttribute("youtube"))
             src = self.getAttribute("youtube");
         document.getElementById("galleryImage").children[1].src = src;
         document.getElementById("galleryImage").children[0].style.display = "none";
         document.getElementById("galleryImage").children[1].style.display = "inherit";
     }
+    // Show Image
+    else {
+        var src = self.style.backgroundImage.substring(5, self.style.backgroundImage.length - 2);
+        src = src.substring(0, src.lastIndexOf('/')) + "/fullres" + src.substring(src.lastIndexOf('/'));
+        document.getElementById("galleryImage").children[0].src = src;
+        document.getElementById("galleryImage").children[0].style.display = "inherit";
+        document.getElementById("galleryImage").children[1].style.display = "none";
+    }
     showOverlay("galleryImage");
     document.body.style.overflowY = "hidden";
+}
+
+//No fullres option
+function reloadGalleryImage(self) {
+    self.src = self.src.substring(0, self.src.lastIndexOf('/') - 8) + self.src.substring(self.src.lastIndexOf('/'));
 }
 
 function switchOrganization(self) {
@@ -204,11 +241,21 @@ function switchOrganization(self) {
         }
     }
 
-    parent = parent.parentNode;
+    if (index == 0)
+        document.location.hash = document.location.hash.split('_')[0];
+    else if (document.location.hash.split('_').length == 1)
+        document.location.hash += "_Demo";
+
+    loadSubDomain(parent.parentNode, index == 1);
+}
+
+function loadSubDomain(parent, isSub)
+{
     for (var i = 0; i < parent.children.length; i++)
         if (parent.children[i].classList.contains("subarticle")) {
-            if ((index == 0 && !parent.children[i].classList.contains("demoProject")) ||
-                (index == 1 && parent.children[i].classList.contains("demoProject"))) {
+            if (((!isSub && !parent.children[i].classList.contains("demoProject")) ||
+                (isSub && parent.children[i].classList.contains("demoProject"))) &&
+                (!parent.children[i].classList.contains("showOnHughes_") || document.title == "Gavin Hughes")) {
                     parent.children[i].classList.remove("hide");
                     parent.children[i].classList.add("showSubarticle");
                 }
@@ -217,4 +264,71 @@ function switchOrganization(self) {
                 parent.children[i].classList.remove("showSubarticle");
             }
         }
+}
+
+//Unfortunately manual setup but oh well
+function setupFavoriteVideos() {
+    var videoInfo = ["How I Became the #1 Living Tombstone Fan!", "liyx33umy78",
+        "[Conquer the Tree] Now Released on AirConsole.com!", "9XybB5iKBDs",
+        "How I Created my First Robot!", "M6HiwVCEiQA",
+        "[VRS] A Virtual Robot Simulator for FTC!", "HvywykxdrBU",
+        "Devlog - Swinging Like Spiderman!", "pgUL1RheLG4",
+        "250 Subscriber S̴p̷e̴c̵̱̓i̵̖̊ḁ̸̈ḻ̷͇̉!̵͚̣̿̔", "K8w44mNalI0",
+        "Making a 3D Plants vs. Zombies Augmented Reality Game!", "rU2_OTNgbO0",
+        "Scrolling RPG Dialogue System in Vanilla Minecraft!", "UCEZmEWEHOY",
+        "[ TRULY EXCELLENT. ] Deltacraft - Deltarune in Minecraft Java Edition", "XXp-lb8CZ7w",
+        "[Villain REWIND] I Made a Cool Game out of Nowhere??", "p2JFJR0HKDA",
+        "Realistic Water in Minecraft?", "DklCVXgwtDg",
+        "Working TNT Yeeter!", "zhM8zV9gDYY",
+        "[Space Fighters!] A New Shoot Em' Up Game", "0KUQqyvHXVk",
+        "SHOW YOURSELF DELTACRAFT! - Deltarune in Minecraft Bedrock Edition", "ucubTxwIqGM",
+        "[Abenteuer] The BETA is FINISHED!", "7Lj53xyhhSA",
+        "Bedrock Commands - Creating a 2D Game: First Demo!", "mMm0U7JsQPo",
+        "Minecraft - Rainbow Six Siege Project!", "Eydxh8v-vNU",
+        "Minecraft Bedrock - Making My Very Own GAME SHOW!", "xTIYSt-OpFg",
+        "My Five Night's At Freddy's Mod!", "ZkBx7dQK9BE",
+        "My First Bedrock Redstone Contraption! - Triangular Piston Door!", "pE4xGnBATzU",
+        "I made Snake, a Memory Game, and Pong on my TI-84 Calculator!", "bsU8hg-PrxA",
+        "Lego Elf on the Shelf!", "RZlHjFEPytI"];
+
+    var playlistElement = document.getElementById("MyBestPlaylist");
+
+    //Add odds then evens
+    for (var i = 0; i < videoInfo.length / 2; i += 2) {
+        addVideo(videoInfo[i * 2], videoInfo[i * 2 + 1]);
+    }
+
+    playlistElement.appendChild(document.createElement("br"));
+
+    for (var i = 1; i < videoInfo.length / 2; i += 2) {
+        addVideo(videoInfo[i * 2], videoInfo[i * 2 + 1]);
+    }
+    
+    //Setup scrolling
+    playlistElement.addEventListener('wheel', (event) => {
+        event.preventDefault(); // Prevent default vertical scrolling
+
+        // Adjust the scroll amount based on deltaY (mouse wheel direction)
+        playlistElement.scrollLeft += event.deltaY;
+    });
+}
+
+function addVideo(videoTitle, videoLink) {
+    //Thumbnail
+    var newVideo = document.createElement("div");
+    newVideo.style.backgroundImage = "url('./assets/sprites/youtube/" + videoTitle.replaceAll('#', '').replaceAll('?', '').replaceAll("'", '').replaceAll(':', '') + ".webp')";
+
+    //Link
+    newVideo.setAttribute("youtube", "https://www.youtube.com/embed/" + videoLink + "?list=PLszFVnnZcmaopY7no2EGotH84xF_lQEoG");
+    newVideo.onclick = function() { showGallery(this); }
+
+    //Title
+    var title = document.createElement("div");
+    title.innerText = videoTitle;
+    newVideo.appendChild(title);
+
+    //Play Bttn
+    newVideo.appendChild(document.getElementById("playBttn").cloneNode(true));
+
+    document.getElementById("MyBestPlaylist").appendChild(newVideo);
 }
